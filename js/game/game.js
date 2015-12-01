36bc08e5feb26a14e3999562189434314f20e1e9
Charis <bob@peershipapp.com>
2015-12-01 21:20:53 +0200

    add game 1

---
+++ js/game/game.js
@@ -0,0 +1,17 @@
+alert('Hello, welcome to the Best Boss game.');
+
+	var userInput = confirm('Are you ready to start?');
+		if (userInput === true){
+			alert("Ok, let's do this !");
+		}
+			var totalscore = 0;
+			function employee(name,position){
+				this.name=name;
+				this.position=position;
+			}
+			var weirdo = new employee("Mark Weirdo", "Programmer");
+			var arrogant = new employee("Steve Arrogant", "Marketing Director");
+			var bigspender = new employee ("Maria Bigspender", "Accountant");
+		else{
+			alert("bye");
+		}
